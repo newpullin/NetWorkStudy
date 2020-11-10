85d98cf62d788204b41b0d88bccce5126ccff9a2
Yongsoo Na <newpullin@naver.com>
2020-11-10 12:14:02 +0900

    Create packetAnalyze.c
    IP packet analyze and print

---
+++ packetAnalyze.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#define IP_HEADER_LEN          1
+#define IP_TOS_LEN             1
+#define IP_TL_LEN              2
+#define IP_IDE_LEN             2
+#define IP_FRAGMENTATION_LEN   2
+#define IP_TTL_LEN             1
+#define IP_PROTOCOL_LEN        1
+#define IP_HEADER_CHECKSUM_LEN 2
+#define IP_SOURCE_ADDR_LEN     4
+#define IP_DEST_ADDR_LEN       4
+
+#define IP_HEADER              0
+#define IP_TOS                 (IP_HEADER + IP_HEADER_LEN)
+#define IP_TL                  (IP_TOS + IP_TOS_LEN)
+#define IP_IDENTIFICATION      (IP_TL + IP_TL_LEN)
+#define IP_FRAGMENTATION       (IP_IDENTIFICATION + IP_IDE_LEN)
+#define IP_TTL                 (IP_FRAGMENTATION + IP_FRAGMENTATION_LEN)
+#define IP_PROTOCOL            (IP_TTL + IP_TTL_LEN)
+#define IP_HEADER_CHECKSUM     (IP_PROTOCOL + IP_PROTOCOL_LEN)
+#define IP_SOURCE_ADDR         (IP_HEADER_CHECKSUM + IP_HEADER_CHECKSUM_LEN)
+#define IP_DEST_ADDR           (IP_SOURCE_ADDR + IP_SOURCE_ADDR_LEN)
+
+#define VAL_LEN 10
+
+enum Values {
+    Version = 0,
+    IHL,
+    TOS,
+    TL,
+    Identification,
+    Flag,
+    FragmentOffset,
+    TTL,
+    Porotocol,
+    HeaderChecksum,
+};
+
+const char* value_string[] = {"Version", "IHL", "TOS", "TL", "Identification", "Flag", "FragmentOffset", "TTL", "Porotocol", "HeaderChecksum"};
+
+
+const unsigned char netstr[] = {0x45, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x40, 0x00, 0x35, 0x11, 0xbf, 0x3a, 0xac, 0xd9, 0x18, 0x42, 0x00, 0x10, 0xc0, 0xa8, 0x00, 0xa5};
+
+
+void printIP(const unsigned char* arr, int start){
+    for(int i = 0; i < 3; i ++) {
+        printf("%d.", *(arr+start+i));
+    }
+    printf("%d\n", *(arr+start+3));
+}
+
+void parsing(int* result, const unsigned char* netstr) {
+    result[Version] = (netstr[IP_HEADER ])>>4;
+    result[IHL] = ((netstr[IP_HEADER ])&0x0F) * 4;
+    result[TOS] = netstr[IP_TOS] >> 4;
+    result[TL] = (netstr[IP_TL] * 16 ) + netstr[IP_TL+1];
+    result[Identification] = netstr[IP_IDENTIFICATION ] * 16 + netstr[IP_IDENTIFICATION +1];
+    result[Flag] = netstr[IP_FRAGMENTATION] >> 5 ;
+    result[FragmentOffset] = (netstr[IP_FRAGMENTATION] & 0x1f)*(0xff) + netstr[IP_FRAGMENTATION+1];
+    result[TTL] = netstr[IP_TTL];
+    result[Porotocol] = netstr[IP_PROTOCOL];
+    result[HeaderChecksum] = netstr[IP_HEADER_CHECKSUM];
+
+    for(int i = 0; i < VAL_LEN; i++) {
+        printf("%s : %d \n", value_string[i], result[i]);
+    }
+
+    printf("Source Address: ");
+    printIP(netstr, IP_SOURCE_ADDR);
+    printf("Destination Address: ");
+    printIP(netstr, IP_DEST_ADDR);
+}
+
+int main(void){
+
+    int result[VAL_LEN];
+    parsing(result, netstr);
+
+    return 0;
+}
